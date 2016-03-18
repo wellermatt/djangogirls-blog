7746c0d22514e336ac141f716a4a2724e04f821b
Matt Weller <wellermatt@hotmail.com>
2016-03-18 09:31:35 +0000

    Added the review screen

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -18,7 +22,7 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else :
